af004a447c86c0df1486b44dbbe2a29347cc4b52
XaiXaiii365 <delosreyes.368742@pasay-edsa.sti.edu.ph>
2025-05-12 18:04:31 +0800

    Delete agrisocialapp directory

--- agrisocialapp/apps.py
+++
@@ -1,8 +0,0 @@
-from django.apps import AppConfig
-
-class AgrisocialappConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'agrisocialapp'
-
-    def ready(self):
-        import agrisocialapp.signals  # Ensure signals are imported
